2c6b78a905ccaf8cda1bccdc6dc9ef325f0774e7
Brett Smith <xbcsmith@gmail.com>
2024-02-17 10:45:07 -0500

    docs: add fedora 39 guide

---
+++ apps/balena-etcher/updater.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+VERSION="${1:-1.5.114}"
+ORG="balena-io"
+PROJECT="etcher"
+APPNAME="balenaEtcher"
+ARCH="x64"
+BINARY="https://github.com/${ORG}/${PROJECT}/releases/download/v${VERSION}/${APPNAME}-${VERSION}-${ARCH}.AppImage"
+
+echo "updating ${APPNAME} to ${VERSION} ${BINARY}"
+
+wget -O ${APPNAME}-${VERSION}-${ARCH}.AppImage --show-progress --progress=bar:force:noscroll ${BINARY}
+chmod +x ${APPNAME}-${VERSION}-${ARCH}.AppImage
+sed -e "s/@VERSION@/${VERSION}/g" ${APPNAME,,}.desktop.tmpl > ${APPNAME,,}.desktop
+cp -v ${APPNAME,,}.desktop ~/.local/share/applications/${APPNAME,,}.desktop
